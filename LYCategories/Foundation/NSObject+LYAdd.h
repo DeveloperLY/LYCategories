98c549da6aefc2ae7e4a30bc9582ea64e231cd16
LY <coderyliu@gmail.com>
2017-03-26 23:43:08 +0800

    Update NSObject+LYAdd

--- LYCategories/Foundation/NSObject+LYAdd.h
+++ LYCategories/Foundation/NSObject+LYAdd.h
@@ -23,12 +23,57 @@
 ///=============================================================================
 
 /**
- *  消息发送选择器
+ *  @brief消息发送选择器
  *
  *  @param sel 变量参数列表
  *
  *  @return 消息结果
  */
 - (id)performSelector:(SEL)aSelector withObjects:(NSArray *)objects;
+
+#pragma mark - AssociatedObject 附加属性
+///=============================================================================
+/// @name AssociatedObject 附加属性
+///=============================================================================
+
+/**
+ *  @brief  附加一个Stong对象
+ *
+ *  @param value 被附加的对象
+ *  @param key   被附加对象的key
+ */
+- (void)ly_associateValue:(id)value withKey:(void *)key;
+
+/**
+ *  @brief  附加一个weak对象
+ *
+ *  @param value 被附加的对象
+ *  @param key   被附加对象的key
+ */
+- (void)ly_weaklyAssociateValue:(id)value withKey:(void *)key;
+
+/**
+ *  @brief  根据附加对象的key取出附加对象
+ *
+ *  @param key 附加对象的key
+ *
+ *  @return 附加对象
+ */
+- (id)ly_associatedValueForKey:(void *)key;
+
+#pragma mark - AddProperty 添加属性
+///=============================================================================
+/// @name AddProperty 添加属性
+///=============================================================================
+
+/**
+ *  @brief  catgory runtime实现get set方法增加一个字符串属性
+ */
+@property (nonatomic, strong) NSString *ly_stringProperty;
+
+/**
+ *  @brief  catgory runtime实现get set方法增加一个NSInteger属性
+ */
+@property (nonatomic, assign) NSInteger ly_integerProperty;
 
 @end
--- LYCategories/Foundation/NSObject+LYAdd.m
+++ LYCategories/Foundation/NSObject+LYAdd.m
@@ -11,6 +11,13 @@
 
 LYSYNTH_DUMMY_CLASS(NSObject_LYAdd)
 
+/** 
+ *  objc_getAssociatedObject和objc_setAssociatedObject都需要指定一个固定的地址，
+ *  这个固定的地址值用来表示属性的key，起到一个常量的作用。
+ */
+static const void *LYStringProperty = &LYStringProperty;
+static const void *LYIntegerProperty = &LYIntegerProperty;
+
 @implementation NSObject (LYAdd)
 
 #pragma mark - NSInvocation封装
@@ -46,6 +53,54 @@ LYSYNTH_DUMMY_CLASS(NSObject_LYAdd)
         [invocation getReturnValue:&res];
     }
     return res;
+}
+
+#pragma mark - AssociatedObject 附加属性
+
+- (void)ly_associateValue:(id)value withKey:(void *)key {
+    objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (void)ly_weaklyAssociateValue:(id)value withKey:(void *)key {
+    objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (id)ly_associatedValueForKey:(void *)key {
+    return objc_getAssociatedObject(self, key);
+}
+
+#pragma mark - AddProperty 添加属性
+@dynamic ly_stringProperty;
+@dynamic ly_integerProperty;
+
+//set
+/**
+ *  @brief  catgory runtime实现get set方法增加一个字符串属性
+ */
+- (void)setLy_stringProperty:(NSString *)ly_stringProperty {
+    //use that a static const as the key
+    objc_setAssociatedObject(self, LYStringProperty, ly_stringProperty, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    //use that property's selector as the key:
+    //objc_setAssociatedObject(self, @selector(stringProperty), stringProperty, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+//get
+- (NSString *)ly_stringProperty {
+    return objc_getAssociatedObject(self, LYStringProperty);
+}
+
+//set
+/**
+ *  @brief  catgory runtime实现get set方法增加一个NSInteger属性
+ */
+- (void)setLy_integerProperty:(NSInteger)ly_integerProperty {
+    NSNumber *number = [[NSNumber alloc]initWithInteger:ly_integerProperty];
+    objc_setAssociatedObject(self,LYIntegerProperty, number, OBJC_ASSOCIATION_ASSIGN);
+}
+
+//get
+- (NSInteger)ly_integerProperty {
+    return [objc_getAssociatedObject(self, LYIntegerProperty) integerValue];
 }
 
 @end
