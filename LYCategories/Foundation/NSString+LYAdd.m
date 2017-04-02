bdcecb33e012f30419ec6eaae92addcd76261f19
LY <coderyliu@gmail.com>
2017-04-02 15:43:59 +0800

    Update NSString+LYAdd

--- LYCategories/Foundation/NSString+LYAdd.m
+++ LYCategories/Foundation/NSString+LYAdd.m
@@ -671,4 +671,27 @@ LYSYNTH_DUMMY_CLASS(NSString_LYAdd)
     return buffer;
 }
 
+#pragma mark - 拼音相关
+- (instancetype)hanzi2pinyin {
+    NSMutableString *str = [self mutableCopy];
+    CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformMandarinLatin, NO);
+    CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformStripDiacritics, NO);
+    
+    return [str stringByReplacingOccurrencesOfString:@" " withString:@""];
+}
+
+- (instancetype)pinyinInitial {
+    NSMutableString *str = [self mutableCopy];
+    CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformMandarinLatin, NO);
+    CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformStripDiacritics, NO);
+    
+    NSArray *word = [str componentsSeparatedByString:@" "];
+    NSMutableString *initial = [[NSMutableString alloc] initWithCapacity:str.length / 3];
+    for (NSString *str in word) {
+        [initial appendString:[str substringToIndex:1]];
+    }
+    
+    return initial;
+}
+
 @end
